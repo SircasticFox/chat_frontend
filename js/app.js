3dcd02631b062f98695d6e4177d3ea0ba8fe024f
jpbest <it>
2017-06-27 10:24:30 +0200

    Basic WS structure

--- js/app.js
+++ js/app.js
@@ -3,4 +3,23 @@
  */
 (function() {
     var app = angular.module('chat', ['ngMaterial']);
+
+    var socket = new WebSocket('wss://echo.websocket.org');
+    socket.onopen = function (event) {
+        //Connection opens
+    };
+
+    socket.onclose = function (event) {
+        //Connection closes
+    };
+
+    socket.onerror = function (event) {
+        //Some kind of error
+    };
+
+    socket.onmessage = function (event) {
+        //new message from server
+    };
+
+
 })();
