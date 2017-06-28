c35b038d4f81f5837f996d7b760fa2dd1367784e
Andreas Fuchs <Andrej.Fuchs@gmail.com>
2017-06-28 10:34:40 +0200

    Implemented Basic Chatroom UI, still some problems with the correct display of the input area

--- README.md
+++ README.md
@@ -1,12 +1,17 @@
 # Chat Frontend
 Assignment for the Webengineering Class @ DHBW Stuttgart
 
-# Chat Backend
-The backend was provided and can be found here: https://github.com/Lhdang88/cloud_computing_ws
+## Chat Backend
+The backend was provided and can be found here:
+https://github.com/Lhdang88/cloud_computing_ws
 
-## Angular Material:
+### Angular Material:
 https://material.angularjs.org/latest/
 
-## Angular Websockets:
+### Angular Websockets:
+(not used for now)
 https://github.com/AngularClass/angular-websocket
+
+### Icons
+All used Icons are from https://material.io/icons/
 
--- js/app.js
+++ js/app.js
@@ -4,6 +4,12 @@
 (function() {
     var app = angular.module('chat', ['ngMaterial']);
 
+    //Create Chat Controller, which holds the messages and the rooms
+    app.controller('chatController', function () {
+        this.messages = dummyMessages;
+        this.rooms = dummyRooms;
+    });
+
     var socket = new WebSocket('wss://echo.websocket.org');
     socket.onopen = function (event) {
         //Connection opens
@@ -22,4 +28,41 @@
     };
 
 
+    var dummyMessages = [
+        {
+            user: "User",
+            message: "Message 1",
+            timestamp: 1397490980837
+        },
+        {
+            user: "User",
+            message: "Message 2",
+            timestamp: 1397490980837
+        },
+        {
+            user: "User",
+            message: "Message 3",
+            timestamp: 1397490980837
+        },
+        {
+            user: "User",
+            message: "Message 4",
+            timestamp: 1397490980837
+        }
+    ];
+
+    var dummyRooms = [
+        {
+            room: "Lobby"
+        },
+        {
+            room: "Room 1"
+        },
+        {
+            room: "Room 2"
+        },
+        {
+            room: "Room 3"
+        }
+    ];
 })();
