7252e0d2cc6d8e7bbff0bcd658863c892d6a5d80
jpbest <it>
2017-07-02 13:02:48 +0200

    added simple message send

--- js/app.js
+++ js/app.js
@@ -4,9 +4,13 @@
 (function() {
     var app = angular.module('chat', ['ngMaterial']);
 
-    var socket = new WebSocket('wss://echo.websocket.org');
+    var socket = new WebSocket('ws://liebknecht.danielrutz.com:3000/chat');
     socket.onopen = function (event) {
-        //Connection opens
+        console.log("Connected!");
+        postPrivateMessage("Lobby", "user282948249", "firstMEssage");
+        postPublicMessage("Lobby", "user292929", "secondMessage");
+        refreshAllRooms();
+        refreshUsersInRoom(2);
     };
 
     socket.onclose = function (event) {
@@ -18,7 +22,55 @@
     };
 
     socket.onmessage = function (event) {
-        //new message from server
+       // var json_response = JSON.parse(event.data);
+        //console.log(json_response);
+        console.log(event.data);
     };
-//test
+
+
+    function refreshAllRooms(){
+        var payload = {
+            "action": "getChatRooms"
+        }
+        var jString = JSON.stringify(payload);
+        console.log(jString);
+
+        socket.send(jString);
+    };
+
+    function refreshUsersInRoom(roomId) {
+        var payload = {
+            "action": "getUsersInRoom",
+            "roomId": roomId
+        }
+        socket.send(JSON.stringify(payload));
+
+    }
+
+    function postPrivateMessage(targetUID, user_source, message){
+        var content = {
+            "action": "postPrivateMessage",
+            "userId": targetUID,
+            "user": user_source,
+            "message": message,
+            "meta": { "foo": "bar"} // additional data to pass
+        }
+        console.log(JSON.stringify(content));
+        socket.send(JSON.stringify(content));
+
+    }
+
+    function postPublicMessage(roomId, user_source, message){
+        var content = {
+            "action": "postMessage",
+            "roomId": roomId,
+            "user": user_source,
+            "message": message,
+            "meta": { "foo": "bar"} // additional data to pass
+        }
+        console.log(JSON.stringify(content));
+        socket.send(JSON.stringify(content));
+    }
+
+
 })();
