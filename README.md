c6f3de91f5f01c61629bf316d0c0eff8f37a2429
Andreas Fuchs <andrej.fuchs@gmail.com>
2017-07-05 20:20:44 +0200

    Added angular websocket module, scroll module & and connected the frontend with the backend, sending messages works

--- README.md
+++ README.md
@@ -7,11 +7,6 @@ The [Backend] was provided.
 ### Angular Material:
 https://material.angularjs.org/latest/
 
-### Angular Websockets:
-(not used for now)
-
-https://github.com/AngularClass/angular-websocket
-
 ### Icons
 All Icons are [Material Icons]
 
@@ -21,11 +16,15 @@ To create this chat the following libraries were used:
 
 * [AngularJS] - HTML UI Framework
 * [Angular Material] - reference implementation of the Material Design for AngularJS
+* [Angular Websocket] - Websocket Module for AngularJS
 * [Material Icons] - Huge amount of great Material Icons
 * [JQuery Emoji Picker] - Provides a popup Window for selecting Emojis
+* [angularjs-scroll-glue] - Scrolls Automatically on new Messages
 
 [AngularJS]: <http://angularjs.org>
 [Angular Material]: <http://material.angularjs.org>
 [Material Icons]: <https://material.io/icons/>
 [Backend]: <https://github.com/Lhdang88/cloud_computing_ws>
 [JQuery Emoji Picker]: <https://www.npmjs.com/package/jquery-emoji-picker>
+[angularjs-scroll-glue]: <https://github.com/Luegg/angularjs-scroll-glue>
+[Angular Websocket]: <https://github.com/AngularClass/angular-websocket>
--- js/app.js
+++ js/app.js
@@ -2,334 +2,127 @@
  * Created by Andreas on 26.06.2017.
  */
 (function() {
-    var app = angular.module('chat', ['ngMaterial', 'custom-directives']);
+    var app = angular.module('chat', ['ngMaterial', 'ngWebSocket', 'ngAnimate', 'custom-directives', 'luegg.directives']);
+
+    app.factory('ws', function($websocket) {
+        // Open a WebSocket connection
+        var baseUrl = "5.45.105.154:3000";
+        var wsBaseUrl = "ws://" + baseUrl + "/chat";
+        var socket = $websocket(wsBaseUrl);
+
+        socket.onMessage(function(message) {
+            wsCalls.onMessageListener(message);
+        });
+        socket.onOpen(function (message) {
+            console.log("Opened Websocket Connection");
+            wsCalls.getRooms();
+        });
+        socket.onClose(function (message) {
+            console.log("Closed Websocket Connection");
+        });
+
+        var wsCalls = {
+            onMessageListener: null,
+            getRooms: function() {
+                socket.send(JSON.stringify({
+                    "action": "getChatRooms"
+                }));
+            },
+            getMessages: function (room) {
+                socket.send(JSON.stringify({
+                    "action": "getMessagesInRoom",
+                    "roomId": room
+                }));
+            },
+            sendPublicMessage: function (room, message, sender) {
+                socket.send(JSON.stringify({
+                    "action": "postMessage",
+                    "roomId": room,
+                    //"userId": "receiverUser_for_postPrivateMessage",
+                    "user": sender,
+                    "message": message,
+                    "meta": { "": ""} // additional data to pass
+                }));
+            },
+            sendPrivateMessage: function (receiver, message, sender) {
+                socket.send(JSON.stringify({
+                    "action": ["postPrivateMessage"],
+                    "userId": receiver,
+                    "user": sender,
+                    "message": message,
+                    "meta": { "": ""} // additional data to pass
+                }));
+            }
+        };
+
+        return wsCalls;
+    });
 
     //Create Chat Controller, which holds the messages and the rooms
-    app.controller('chatController', function () {
-        this.myMessage = "";
-        this.myRoom = "Lobby";
-        this.messages = dummyMessages;
-        this.rooms = dummyRooms;
-        this.openRoom = function (room) {
-            console.log("Joining Room: " + room.room);
+    app.controller('chatController', function ($scope, ws) {
+        //Websocket Events
+        this.socket = ws;
+        ws.onMessageListener = function(message){
+            console.log("Received Message: " + message.data);
+            var data = JSON.parse(message.data);
+            //Room Data
+            if(data.length > 0 && !data[0].hasOwnProperty('user')) {
+                //Adding Rooms to List
+                data.forEach(function (room) {
+                    $scope.rooms.push(room);
+                });
+            }
+            else if(data.hasOwnProperty('action')){
+                var newMessage = {
+                    "user": data.user,
+                    "message": data.message,
+                    "timestamp": Date.now()
+                };
+                $scope.messages.push(newMessage);
+            }
+            //Message Data
+            else if(data.length > 0){
+                data.forEach(function (mes) {
+                    $scope.messages.push(mes);
+                });
+            }
         };
+
+        //Controller Data
+        $scope.rooms = [];
+        $scope.messages = [];
+        $scope.myRoom = "";
+        $scope.myUser = "Hackerman";
+        $scope.myMessage = "";
+
+        this.joinRoom = function (index) {
+            var room = $scope.rooms[index];
+            $scope.myRoom = room;
+            //Clean Messages
+            $scope.messages = [];
+            //request messages for that specific room
+            ws.getMessages(room);
+            console.log("Joining Room: " + room);
+        };
+
         this.sendMessage = function () {
-            
+            //Sending Message to websocket
+            console.log("Sending Message: " + $scope.myMessage);
+            ws.sendPublicMessage($scope.myRoom, $scope.myMessage, $scope.myUser);
+            //Reset my Message after Sending
+            $scope.myMessage = "";
         };
         this.getFirstLetter = function(input) {
             return input.substring(0,1);
         };
-        this.scrollOnNew = function(scope, element){
-            scope.$watchCollection('this.messages', this.scrollDown);
-        };
-        this.scrollDown = function(){
-            var list = angular.element( document.querySelector( '#chat-content' ) );
-            var scrollH = list.prop('scrollHeight');
-            list.animate({scrollTop: scrollH}, 500);
-        };
         this.emojiClick = function(){
             $('#messageInput').emojiPicker({
-                width: '600px',
-                height: '350px',
+                width: '400px',
+                height: '200px',
                 button: false
-
             });
             $('#messageInput').emojiPicker('toggle');
         };
     });
-
-
-
-
-    //Connection URLs, might add a feature to change it in the Client
-    var baseUrl = "5.45.105.154:3000";
-    //var baseUrl = "localhost:3000"
-    var restBaseUrl = "http://" + baseUrl + "/api";
-    var wsBaseUrl = "ws://" + baseUrl + "/chat";
-
-    var CC = new ChatContent();
-    LoadRooms();
-
-    //Websocket and all its events
-    var socket = new WebSocket(wsBaseUrl);
-
-    socket.onopen = function (event) {
-        var createChannels = {
-            "action": "postMessage",
-            "roomId": "Lobby",
-            //"userId": "receiverUser_for_postPrivateMessage",
-            "user": "Bot",
-            "message": "test",
-            "meta": {"": ""} // additional data to pass
-            };
-        socket.send(JSON.stringify(createChannels));
-    };
-
-    socket.onclose = function (event) {
-        //Connection closes
-    };
-
-    socket.onerror = function (event) {
-        //Some kind of error
-    };
-
-    socket.onmessage = function (event) {
-        console.log(event.data);
-    };
-
-     function ChatRoomData(roomID, users, messagesInRoom){
-
-        this.roomID  = roomID;
-        this.usersInRoom = users;
-        this.messagesInRoom = messagesInRoom;
-
-        //checks if user is already recognised in the current room, if not, he/she will get added
-        this.checkUser =  function (userID) {
-            var exist = false;
-            for(user of this.usersInRoom){
-                if(userID == user){
-                    exist = true;
-                }
-            }
-            if(!exist){
-                this.usersInRoom.push(userID);
-            }
-        };
-
-        this.incommingMessage = function (msg) {
-            this.checkUser(msg.user);
-            this.messagesInRoom.push(msg);
-
-
-        }
-    }
-
-    function ChatContent() {
-        this.rooms = [];
-    }
-
-    function LoadRooms() {
-
-        //load all Rooms
-        var opts = {
-            method: 'GET',
-            headers: {
-                'Authorization': 'Basic ' + btoa('dhbw:dhbw-pw'),
-                'Content-Type': 'application/x-www-form-urlencoded'
-            }
-        };
-        fetch(restBaseUrl + "/chats", opts).then(function (response) {
-            return response.json();
-        }).then(function (body) {
-
-            for (channel of body) {
-                CC.rooms.push(new ChatRoomData(channel, [], []));
-            }
-
-            LoadMessages();
-        });
-
-
-
-    }
-
-    function LoadMessages(){
-        //load all messages and so on
-        var opts = {
-            method: 'GET',
-            headers: {
-                'Authorization': 'Basic ' + btoa('dhbw:dhbw-pw'),
-                'Content-Type': 'application/x-www-form-urlencoded'
-            }
-        };
-        for(room of CC.rooms) {
-
-            fetch(restBaseUrl + "/chats/" + room.roomID, opts).then(function (response) {
-                return response.json();
-
-            }).then(function (body) {
-                console.log("loading messages for room: " + room.roomID);
-                for(msg of body){
-                    console.log(msg);
-                    room.incommingMessage(msg);
-                }
-
-            });
-        }
-        LoadUsers();
-    }
-
-    function LoadUsers() {
-        //load all Users and so on
-        var opts = {
-            method: 'GET',
-            headers: {
-                'Authorization': 'Basic ' + btoa('dhbw:dhbw-pw'),
-                'Content-Type': 'application/x-www-form-urlencoded'
-            }
-        };
-        for(room of CC.rooms) {
-            fetch(restBaseUrl + "/chats/" + room.roomID + "/users", opts).then(function (response) {
-                return response.json();
-
-            }).then(function (body) {
-                for(usr of body){
-                    room.checkUser(msg);
-
-                }
-            });
-        }
-        console.log(CC);
-
-    }
-
-
-
-    function refreshAllRooms(){
-        var payload = {
-            "action": "getChatRooms"
-        }
-        var jString = JSON.stringify(payload);
-        console.log(jString);
-
-        socket.send(jString);
-    };
-
-    function refreshUsersInRoom(roomId) {
-        var payload = {
-            "action": "getUsersInRoom",
-            "roomId": roomId
-        }
-        socket.send(JSON.stringify(payload));
-
-    }
-
-    function postPrivateMessage(targetUID, user_source, message){
-        var content = {
-            "action": "postPrivateMessage",
-            "userId": targetUID,
-            "user": user_source,
-            "message": message,
-            "meta": { "foo": "bar"} // additional data to pass
-        }
-        console.log(JSON.stringify(content));
-        socket.send(JSON.stringify(content));
-
-    }
-
-    function postPublicMessage(roomId, user_source, message){
-        var content = {
-            "action": "postMessage",
-            "roomId": roomId,
-            "user": user_source,
-            "message": message,
-            "meta": { "foo": "bar"} // additional data to pass
-        }
-        console.log(JSON.stringify(content));
-        socket.send(JSON.stringify(content));
-    }
-
-
-    var dummyMessages = [
-        {
-            user: "User",
-            message: "Message 1s 😂",
-            timestamp: 1397490980837
-        },
-        {
-            user: "User",
-            message: "Message 2 😂",
-            timestamp: 1397490980837
-        },
-        {
-            user: "User",
-            message: "Message 3 😂",
-            timestamp: 1397490980837
-        },
-        {
-            user: "User",
-            message: "Message 4",
-            timestamp: 1397490980837
-        },
-        {
-            user: "User",
-            message: "Message 5",
-            timestamp: 1397490980837
-        },
-        {
-            user: "User",
-            message: "Message 6",
-            timestamp: 1397490980837
-        },
-        {
-            user: "User",
-            message: "Message 7",
-            timestamp: 1397490980837
-        },
-        {
-            user: "User",
-            message: "Message 8",
-            timestamp: 1397490980837
-        },
-        {
-            user: "User",
-            message: "Message 9",
-            timestamp: 1397490980837
-        }
-        ,
-        {
-            user: "User",
-            message: "Message 10",
-            timestamp: 1397490980837
-        }
-    ];
-
-    var dummyRooms = [
-        {
-            room: "Lobby",
-            lastUser: "User1",
-            lastMessage: "test"
-        },
-        {
-            room: "Room 1",
-            lastUser: "User2",
-            lastMessage: "test2"
-        },
-        {
-            room: "Room 2",
-            lastUser: "User3",
-            lastMessage: "test3"
-        },
-        {
-            room: "Room 3",
-            lastUser: "User3",
-            lastMessage: "test3"
-        },
-        {
-            room: "Room 4",
-            lastUser: "User3",
-            lastMessage: "test3"
-        },
-        {
-            room: "Room 5",
-            lastUser: "User3",
-            lastMessage: "test3"
-        },
-        {
-            room: "Room 6",
-            lastUser: "User3",
-            lastMessage: "test3"
-        },
-        {
-            room: "Room 7",
-            lastUser: "User3",
-            lastMessage: "test3"
-        },
-        {
-            room: "Room 8",
-            lastUser: "User3",
-            lastMessage: "test3"
-        }
-    ];
 
 })();
